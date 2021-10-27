c927bcaff00ed28678448d9d01c2da3ddffda056
Snigdhasai <51473164+Snigdhasai@users.noreply.github.com>
2021-10-27 10:14:07 +0530

    Add files via upload

---
+++ WebForm4.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm4.aspx.cs" Inherits="CachingDemo.WebForm4" %>
+<%--<%@ OutputCache Duration="10" VaryByParam="TextBox1;TextBox2;TextBox3" %>--%>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>
+                Enter 1stNo:&nbsp;
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                Enter 1stNo:&nbsp;
+                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add" />
+            &nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Show the cached output" />
+                <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            </p>
+            <hr />
+            <p>
+                &nbsp;</p>
+        </div>
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="File dependency" />
+&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+    &nbsp;
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Remove" />
+    </form>
+</body>
+</html>
---
+++ WebForm4.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Caching;
+
+namespace CachingDemo
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {/*
+            int ans = Convert.ToInt32(TextBox1.Text) + Convert.ToInt32(TextBox2.Text);
+            TextBox3.Text = ans.ToString();*/
+            //Cache["answer"] = TextBox3.Text;
+            //Cache.Add("answer", TextBox3.Text, null, 
+            //    Cache.NoAbsoluteExpiration, TimeSpan.FromSeconds(10),CacheItemPriority.Default, null);
+
+            //CacheItemRemovedCallback cb = new CacheItemRemovedCallback(removing);
+
+            //2.10.20pm---- 
+            //    10--absolute expiration-----2.10.30pm
+            //    10--sliding----2.10.24pm----2.10.34pm
+
+
+            //--absolute----exact time the cache will outdated
+            //    --sliding ----- 
+        }
+        private void removing(string key, object val, CacheItemRemovedReason r)
+        {
+
+           // r = CacheItemRemovedReason.DependencyChanged;
+            // Response.Write(TextBox5.Text);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+              //TextBox4.Text = Cache["answer"].ToString();
+            //TextBox4.Text = Cache.Get("answer").ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            CacheDependency dep = new CacheDependency(@"C:\Users\D.SAI SNIGDHA\Desktop\Snigdha\Mphasis\198-Mphasis\ASP.NET\CachingDemo\CachingDemo\Demo.txt");
+
+            string fname = @"C:\Users\D.SAI SNIGDHA\Desktop\Snigdha\Mphasis\198-Mphasis\ASP.NET\CachingDemo\CachingDemo\Demo.txt";
+            Cache.Add("myfile", fname, dep, DateTime.Now.AddSeconds(1), Cache.NoSlidingExpiration, CacheItemPriority.High, new CacheItemRemovedCallback(removing));
+
+            CacheItemRemovedReason r;
+            if (Cache["myfile"] == null)
+            {
+                r = CacheItemRemovedReason.Expired;
+                TextBox5.Text = r.ToString();
+            }
+            else
+            {
+                r = CacheItemRemovedReason.DependencyChanged;
+                TextBox5.Text = r.ToString();
+            }
+            
+            
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Cache.Remove("myfile");
+            CacheItemRemovedReason r;
+            r = CacheItemRemovedReason.Removed;
+            TextBox5.Text = r.ToString();
+        }
+    }
+}
